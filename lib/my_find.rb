6bb21dd00a3d6555e499a2fe128c3d903177c10d
vy vo <tuonggvy.vo@gmail.com>
2019-11-08 03:50:29 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 def my_find(collection)
  i = 0 
- while i <
- collection.length
-    yield(collection[i])
+ while i < collection.length
+    return collection[i] if yield(collection[i])
     i = i + 1
   end
 end
